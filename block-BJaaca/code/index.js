4d5960afa6c6d9697bdbbf9b0c0b40ce6a80d8c8
Jacob Joel <joezy28@gmail.com>
2022-04-07 16:32:22 +0530

    data structure js

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -2,6 +2,8 @@
 
 //  - Create a variable named `character` using `let` assign a value of empty object.
 
+let character = {};
+
 //  - Go to this [Arya Stark](https://awoiaf.westeros.org/index.php/Arya_Stark)
 
 //  Re-assign the value of `character` variable to and object with the key `characterName` and value of "Arya"
